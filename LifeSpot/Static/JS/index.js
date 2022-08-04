188ed615dd65ff21547581663461d2f2657dfa43
Zhilenkov Artem <zhilenkovartem@mail.ru>
2022-08-04 21:53:21 +0300

    first

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,82 @@
+﻿function filterContent() { // Принимаем пользовательский ввод в качестве параметра.
+
+    // Находим контейнеры с видео, которые необходимо фильтровать
+    let elements = document.getElementsByClassName('video-container');
+
+    // Пробегаемся по контейнерам
+    for (let i = 0; i <= elements.length; i++) {
+        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
+        let videoText = elements[i].getElementsByTagName('h3')[0].textContent;//elements[i].querySelector(".video-title").innerText;
+
+        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
+        if (!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())) {
+            // Скрываем неподходящие
+            elements[i].style.display = 'none';
+        } else {
+            // Показываем подходящие
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+/*
+* Функция для фильтрации контента
+* Будет вызываться благодаря атрибуту oninput на index.html
+*
+* */
+/*function filterContent() {
+
+    // Сохраняем текст пользовательского запроса.
+    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+    // Находим контейнеры с видео, которые необходимо фильтровать
+    let elements = document.getElementsByClassName('video-container');
+    // Пробегаемся по контейнерам
+    for (let i = 0; i <= elements.length; i++) {
+        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
+        //let videoText = elements[i].querySelector(".video-title").innerText;
+        let videoText = elements[i].getElementsByTagName('h3')[0].textContent;
+        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
+        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+            // Скрываем неподходящие
+            elements[i].style.display = 'none';
+        } else {
+            // Показываем подходящие
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}*/
+
+/*
+* Сессия теперь создается в общей области видимости.
+* Будет "захватываться" тремя функциями
+*/
+let session = new Map();
+/*
+* Сохранение данных сессии сразу при заходе пользователя на страницу
+*/
+function handleSession() {
+    // Сохраним время начала сессии
+    session.set("startDate", new Date().toLocaleString())
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+}
+/*
+* Проверка возраста пользователя
+*/
+function checkAge() {
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    if (session.get("age") >= 18) {
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+// Логирование сессии (объявлено через function declaration)
+let sessionLog = function logSession() {
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
+}
